f05e3de3fa93b444d4db9869117594d73228a60b
Vasanth <vasanthadabala@gmail.com>
2026-05-06 21:19:09 +0530

    Initial Commit

---
+++ Mayu Echo/Mayu_EchoApp.swift
@@ -0,0 +1,32 @@
+//
+//  Mayu_EchoApp.swift
+//  Mayu Echo
+//
+//  Created by Vasanth on 06/05/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Mayu_EchoApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ Mayu EchoTests/Mayu_EchoTests.swift
@@ -0,0 +1,19 @@
+//
+//  Mayu_EchoTests.swift
+//  Mayu EchoTests
+//
+//  Created by Vasanth on 06/05/26.
+//
+
+import Testing
+@testable import Mayu_Echo
+
+struct Mayu_EchoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
